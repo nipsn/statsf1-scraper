610d231e26c860b4593270c0d9a8c6437980374c
oscar.nydza <o.nydza.2017@alumnos.urjc.es>
2022-08-08 15:29:42 +0200

    Added exception when quali data doesn't exist

--- src/main/scala/DatatableExtractor.scala
+++ src/main/scala/DatatableExtractor.scala
@@ -3,12 +3,21 @@ import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 
 object DatatableExtractor {
+
+  var header: Iterable[String] = Iterable[String]()
+  var data: Iterable[String] = Iterable[String]()
+
   def apply(url: String): (Iterable[String], Iterable[String]) = {
-    val doc = JsoupBrowser().get(url)
-    // TODO: Some grand prix don't have quali data. IE Indianapolis 1953. 1963 upwards doesn't seem to have any issues
-    val data  = (doc >> elementList(".datatable") >> extractor("tbody td", texts)).head
-    val header = (doc >> elementList(".datatable") >> extractor("thead td", texts)).head
+    try {
+      val doc = JsoupBrowser().get(url)
+      data  = (doc >> elementList(".datatable") >> extractor("tbody td", texts)).head
+      header = (doc >> elementList(".datatable") >> extractor("thead td", texts)).head
+    } catch {
+      case e: Exception => println(e + ": Some race didn't have quali data")
+    }
+
     (header, data)
+
   }
 }
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,8 @@
 object Main {
   def main(args: Array[String]): Unit = {
-    (1964 to 2021).toList.foreach(ssn => {
+    (1950 to 2022).toList.foreach(ssn => {
       println(ssn)
       QualifyingData(ssn)
     })
-
   }
 }

f4b5636be089f182a4ded26d9723b8cd6c9227b9
oscar.nydza <o.nydza.2017@alumnos.urjc.es>
2022-08-08 15:57:05 +0200

    Parallelize computation

--- build.sbt
+++ build.sbt
@@ -5,5 +5,6 @@ version := "0.1"
 scalaVersion := "2.13.8"
 
 libraryDependencies := Seq(
-  "net.ruippeixotog" %% "scala-scraper" % "3.0.0"
+  "net.ruippeixotog" %% "scala-scraper" % "3.0.0",
+  "org.scala-lang.modules" %% "scala-parallel-collections" % "1.0.4"
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,17 @@
+import scala.collection.parallel.CollectionConverters._
+
 object Main {
   def main(args: Array[String]): Unit = {
-    (1950 to 2022).toList.foreach(ssn => {
-      println(ssn)
-      QualifyingData(ssn)
+    (1950 to 2022).toList.par.foreach(ssn => {
+      processSeason(ssn)
     })
+  }
+
+
+  def processSeason(ssn: Int): Unit = {
+    println("Searching data for season " + ssn)
+    QualifyingData(ssn)
+    println("Data retrieved for season " + ssn)
+
   }
 }
